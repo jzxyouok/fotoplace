b5fa351bddc7eb1888eef52ce085c6114b68d8ea
fuer4869 <fuer4869@gmail.com>
2016-05-20 14:39:42 +0800

    Initial Commit

---
+++ fotoplace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fotoplace
+//
+//  Created by MBP on 16/5/20.
+//  Copyright © 2016年 MBP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fotoplace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fotoplace
+//
+//  Created by MBP on 16/5/20.
+//  Copyright © 2016年 MBP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
